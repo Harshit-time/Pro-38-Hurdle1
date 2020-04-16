529a31e3e7ec6e88db34f01e60958d2c004ddddd
Harshit-time <59550081+Harshit-time@users.noreply.github.com>
2020-04-16 20:04:57 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,37 @@
+var database;
+
+var gameState = 0;
+
+var playerCount;
+
+var form,player,game;
+
+var allPlayers;
+
+
+
+
+function setup() {
+  createCanvas(800,400);
+
+
+  database = firebase.database();
+
+  game = new Game();
+  game.getState();
+  game.start();
+  
+}
+
+function draw() {
+
+
+  if(playerCount === 4){
+    game.updateState(1);
+  }
+  if(gameState === 1){
+    clear();
+    game.play();
+  }
+ 
+}
